15a8dca43fd38b55e06b569d9c15e40a250977f3
Andres Mtnez <56339735+AndresMtnezGlez@users.noreply.github.com>
2021-06-01 09:11:41 +0200

    Update plmf_functions.R

--- plmf_functions.R
+++ plmf_functions.R
@@ -26,17 +26,17 @@ plot_res_plm <- function (plm_residuals){
                                        (id == 12) ~ "PRT"))
   
   
- plt_res <-  ggplot(data = df_res, aes(year, residuals)) +
+  plt_res <-  ggplot(data = df_res, aes(year, residuals)) +
     geom_line(color = "#41cbb8", size = 1) +
     geom_hline(yintercept=0, linetype="dashed", color = "red")+
     labs(title = "Regression Residuals",
          subtitle = 'EZ Selected countries',
-         y = "Percentaje of GDP", x = "") + 
+         y = "Percentage of GDP", x = "") + 
     facet_wrap(~ cntry,  scales="free") 
-
+  
   l_to_return <- list(plot(plt_res), df_res)
- 
- 
+  
+  
   return(l_to_return)
   
 }
@@ -76,9 +76,9 @@ plot_pred_plm <- function (dependent, model_object) {
     geom_line(aes(y = predicted), color = "#cb4154", size = 1 ) + 
     geom_line(aes(y = model_PDEB), color="#4199cb", size = 1 ) +
     geom_hline(yintercept=60, linetype="dashed", color = "red")+
-    labs(title = "Regression Predicted values (red) and Observed (Blue)",
+    labs(title = "Regression predicted debt level (red) and Observed debt level(blue)",
          subtitle = 'EZ Selected countries',
-         y = "Percentaje of GDP", x = "") + 
+         y = "Percentage of GDP", x = "") + 
     theme(legend.position="bottom")  +
     facet_wrap(~ cntry,  scales="free") 
   
@@ -125,9 +125,9 @@ plot_fitted_by_hand <- function (dependent, fitted_by_hand ) {
     geom_line(aes(y = fitted_by_hand), color = "#cb4154", size = 1) +  
     geom_line(aes(y = model_PDEB), color="#4199cb", size = 1 ) +
     geom_hline(yintercept=60, linetype="dashed", color = "red")+
-    labs(title = "Fitted by hand (red) and observed values(blue)",
+    labs(title = "Structural debt level (red) and Observed debt level(blue)",
          subtitle = 'EZ Selected countries',
-         y = "Percentaje of GDP", x = "") + 
+         y = "Percentage of GDP", x = "") + 
     facet_wrap(~ cntry, scales="free") 
   
 }
@@ -169,4 +169,126 @@ fitted_to_df_var <- function (fitted_by_hand){
 
   return(df_fitted_by_hand_VAR)
 
+}
+
+
+construct_IRFs_CI <- function(x, scales = "free_y", lowerq = 0.16, upperq = 0.84, percentile = 'standard', selection = NULL,
+                                   cumulative = NULL, ..., base)
+  {
+  
+  
+  # Auxiliary functions
+  cutt <- function(x, selind){
+    x$irf <- x$irf[selind]  ## account for the first column being "V1" the horizon counter
+    return(x)
+  }
+  
+  agg <- function(x, aggind){
+    x$irf[,aggind] <- cumsum(x$irf[aggind])
+    return(x)
+  }
+  
+  # To select shocks and variables to plot
+  kk <- ncol(x$true$irf)
+  k <- sqrt(kk - 1)
+  
+  ### Calculate cumulated irfs if necessary
+  if(!is.null(cumulative)){
+    aggind <- list()
+    temp <- (k * cumulative) + 1
+    for(i in 1:length(temp)){
+      aggind[[i]] <- temp[i] - (k - c(1:k))
+    }
+    aggind <- unlist(aggind)
+    
+    x$true      <- agg(x$true, aggind)
+    x$bootstrap <- lapply(x$bootstrap, agg, aggind)
+  }
+  
+  # Select specific shocks or series
+  if(!is.null(selection)){
+    selind <- list()
+    temp <- (k * selection[[1]]) + 1 ## for the column V1
+    for(i in 1:length(temp)){
+      selind[[i]] <- temp[i] - (k - selection[[2]])
+    }
+    selind <- c(1, unlist(selind))
+    
+    x$true      <- cutt(x$true, selind)
+    x$bootstrap <- lapply(x$bootstrap, cutt, selind)
+    
+    kk <- ncol(x$true$irf)
+  }
+  
+  n.ahead <- nrow(x$true$irf)
+  
+  bootstrap <- x$bootstrap
+  nboot <- length(bootstrap)
+  rest <- x$rest_mat
+  
+  n.probs <- length(lowerq)
+  if(length(lowerq) != length(upperq)){
+    stop("Vectors 'lowerq' and 'upperq' must be of same length!")
+  }
+  
+  intervals <- array(0, c(n.ahead, kk, nboot))
+  for(i in 1:nboot){
+    intervals[,,i] <- as.matrix(bootstrap[[i]]$irf)
+  }
+  
+  # find quantiles for lower and upper bounds
+  lower <- array(0, dim = c(n.ahead, kk, n.probs))
+  upper <- array(0, dim = c(n.ahead, kk, n.probs))
+  if(percentile == 'standard' | percentile == 'hall'){
+    for(i in 1:n.ahead){
+      for(j in 1:kk){
+        lower[i,j, ] <- quantile(intervals[i,j, ], probs = lowerq)
+        upper[i,j, ] <- quantile(intervals[i,j, ], probs = upperq)
+      }
+    }
+    
+    if(percentile == 'hall'){
+      for(p in 1:n.probs){
+        lower[ ,-1, p] <- as.matrix(2*x$true$irf)[ ,-1] - lower[ ,-1, p]
+        upper[ ,-1, p] <- as.matrix(2*x$true$irf)[ ,-1] - upper[ ,-1, p]
+      }
+    }
+    
+  }else if(percentile == 'bonferroni'){
+    rest <- matrix(t(rest), nrow = 1)
+    rest[is.na(rest)] <- 1
+    rest <- c(1, rest)
+    for(i in 1:n.ahead){
+      for(j in 1:kk){
+        if(rest[j] == 0){
+          lower[i,j, ] <- quantile(intervals[i,j, ], probs = (lowerq/n.ahead))
+          upper[i,j, ] <- quantile(intervals[i,j, ], probs = 1 + (((upperq - 1)/n.ahead)))
+        }else{
+          lower[i,j, ] <- quantile(intervals[i,j, ], probs = (lowerq/(n.ahead + 1)))
+          upper[i,j, ] <- quantile(intervals[i,j, ], probs = 1 + (((upperq - 1)/(n.ahead + 1))))
+        }
+      }
+    }
+  }else{
+    stop("Invalid choice of percentile; choose between standard, hall and bonferroni")
+  }
+  
+  # plot IRF with confidence bands
+  alp <- 0.7 * (1+log(n.probs, 10))/n.probs
+  irf <- melt(x$true$irf, id = 'V1')
+  cbs <- data.frame(V1 = rep(irf$V1, times=n.probs),
+                    variable = rep(irf$variable, times=n.probs),
+                    probs = rep(1:n.probs, each=(kk-1)*n.ahead),
+                    lower = c(lower[,-1,]),
+                    upper = c(upper[,-1,]))
+  # ggplot() +
+  #   geom_ribbon(data=cbs, aes(x=V1, ymin=lower, ymax=upper, group=probs), alpha=alp, fill='darkgrey') +
+  #   geom_line(data=irf, aes(x=V1, y=value)) +
+  #   geom_hline(yintercept = 0, color = 'red') +
+  #   facet_wrap(~variable, scales = scales, labeller = label_parsed) +
+  #   xlab("Horizon") + ylab("Response") +
+  #   theme_bw()
+  # 
+  return(list(cbs,irf))
+  
 }
